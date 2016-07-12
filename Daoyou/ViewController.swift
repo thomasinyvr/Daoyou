0b5f83e27b8a84b58bb636f96d183a557e5e27b7
thomasinyvr <tomfriesman@gmail.com>
2016-07-12 07:54:06 -0700

    Initial Commit

---
+++ Daoyou/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Daoyou
+//
+//  Created by Thomas Friesman on 2016-07-12.
+//  Copyright © 2016 Thomas Friesman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
